aefd812ddd5dc4f570490ce84d9a514e32e9262d
Aro <aro.jormand@gmail.com>
2024-01-13 09:22:11 +0100

    app-context

---
+++ context/app-context-provider.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { createContext } from "react"
+
+export const AppContext = createContext({})
+
+export const AppContextProvider = ({ children } : {children:React.ReactNode}) => {
+    return <AppContext.Provider value={{}}>{children}</AppContext.Provider>
+}
+
