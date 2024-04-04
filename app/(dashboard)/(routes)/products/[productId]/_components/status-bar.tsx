078ac394a96b199d47da3bdbe4056b6da124b0c3
Aro <aro.jormand@gmail.com>
2024-04-04 22:50:49 +0200

    status-update

--- app/(dashboard)/(routes)/products/[productId]/_components/status-bar.tsx
+++ app/(dashboard)/(routes)/products/[productId]/_components/status-bar.tsx
@@ -23,26 +23,6 @@ export const StatusBar = ({ data, cv }: { data: any; cv: string }) => {
     }
   };
 
-<<<<<<< HEAD
-  const getStatusColor = (status: string) => {
-    switch (status) {
-      case "Draft":
-        return "orange-500";
-      case "Active":
-        return "sky-700";
-      case "Inactive":
-        return "red-500";
-      default:
-        return "gray-400";
-    }}
-
-  return (
-    <div className={`flex items-center gap-4 my-2 pb-2 border-b-2 border-${getStatusColor(data.status)}`}>
-      <h1 className={`font-bold text-${getStatusColor(data.status)}`}>{data.name}</h1>
-      <div className="relative">
-        <button
-          className={`px-4 py-1 rounded-full text-white text-sm font-semibold bg-${getStatusColor(data.status)}`}
-=======
   const getStatusTextColor = (status: string) => {
     switch (status) {
       case "Draft":
@@ -80,7 +60,6 @@ export const StatusBar = ({ data, cv }: { data: any; cv: string }) => {
           className={`px-4 py-1 rounded-full text-white text-sm font-semibold ${getStatusBackgroundColor(
             data.status
           )}`}
->>>>>>> 104494ab344599a95bb203b817609d52ce202e47
           onClick={() => setIsOpen((prev) => !prev)}
         >
           {data.status}
