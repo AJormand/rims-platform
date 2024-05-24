8c3a15b4ddc8789881f252baed0470f2fd7761dc
Aro <aro.jormand@gmail.com>
2024-05-24 23:47:54 +0200

    product-id-registrations-link

--- app/(dashboard)/(routes)/products/[productId]/_components/registration-columns.tsx
+++ app/(dashboard)/(routes)/products/[productId]/_components/registration-columns.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import  Link  from "next/link";
+
 import { ColumnDef } from "@tanstack/react-table";
 
 // This type is used to define the shape of our data.
@@ -14,6 +16,18 @@ export const registrationColumns: ColumnDef<Registration>[] = [
   {
     accessorKey: "name",
     header: "Name",
+    cell: ({ row }) => {
+      const registration = row.original;
+
+      return (
+        <Link
+          href={`/registrations/${registration.id}`}
+          className="underline text-sky-700"
+        >
+          {registration.name}
+        </Link>
+      );
+    },
   },
   {
     accessorKey: "status",
