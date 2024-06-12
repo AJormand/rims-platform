c4aeb10423a3974d14567878e7c53436a313aea9
Aro <aro.jormand@gmail.com>
2024-06-12 23:35:14 +0200

    registration-status-bar-implemented

--- app/(dashboard)/(routes)/registrations/[registrationId]/page.tsx
+++ app/(dashboard)/(routes)/registrations/[registrationId]/page.tsx
@@ -6,13 +6,16 @@ import { Registration as RegistrationType } from "@prisma/client";
 
 import { SideNav } from "@/components/side-nav";
 import { Section } from "@/components/section";
-
 import { DataTable } from "@/components/ui/data-table";
+import { StatusBar } from "@/components/status-bar";
 
 import { BasicDetailsForm } from "./_components/basic-details-form";
 import { productColumns } from "./_components/product-columns";
 
 import { useFetchRegistration } from "@/app/services/hooks/hooks";
+import { editRegistration } from "@/app/services/api-client/api-client";
+
+import { Minimize2, Maximize2 } from "lucide-react";
 
 export default function Registration({
   params,
@@ -101,6 +104,29 @@ export default function Registration({
 
       {data && (
         <div className="w-full px-6 overflow-scroll">
+          <div className="flex border-b-2 py-2 rounded-lg bg-slate-50">
+            <StatusBar
+              data={data.data}
+              cvName={"product-status"}
+              queryKey="registration"
+              editApiFunction={editRegistration}
+            />
+
+            <div className="flex ml-auto gap-1 text-slate-500">
+              <button
+                className="border p-1 rounded-md hover:bg-slate-100"
+                onClick={collapseAllSidenavSections}
+              >
+                <Minimize2 size={15} />
+              </button>
+              <button
+                className="border p-1 rounded-md hover:bg-slate-100"
+                onClick={expandAllSidenavSections}
+              >
+                <Maximize2 size={15} />
+              </button>
+            </div>
+          </div>
           {/* BASIC */}
           <Section
             name="Basic Details"
